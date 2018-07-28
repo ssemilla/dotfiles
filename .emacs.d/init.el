ec016e79d59fc583f5e30f168fdf172334b6aef7
ssemilla <916433+ssemilla@users.noreply.github.com>
2018-07-28 21:30:49 +0800

    Added emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,29 @@
+;;----------------------------------------------------------------
+;; ssemilla's init.el/.emacs file
+
+;;----------------------------------------------------------------
+;; MELPA and friends
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;;----------------------------------------------------------------
+;; Parse and load my actual config (.org -> .el)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;;----------------------------------------------------------------
+;; DO NOT EDIT!!!
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (which-key use-package solarized-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
